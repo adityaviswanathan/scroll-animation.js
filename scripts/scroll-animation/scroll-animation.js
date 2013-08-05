5ae1146fce91ec77e21d764801ec578f524ba048
Aditya Viswanathan <aditya@adityaviswanathan.com>
2013-08-04 21:47:28 -0700

    starting to write core js animation file

--- scripts/scroll-animation/scroll-animation.js
+++ scripts/scroll-animation/scroll-animation.js
@@ -1,1 +1,24 @@
 skrollr.init({ smoothScrolling: true });
+
+function linGrowthRight(classListener, speed, size, color) {
+	
+	globalPosition = $(window).scrollTop();
+
+	var scrollPositionStart = globalPosition;
+
+	var scrollPositionEnd = scrollPositionStart + (speed * 100);
+
+	var sizeSet = size * 10;
+
+	var doubleSizeSet = 2 * sizeSet;
+
+
+	$('.' + classListener).data(scrollPositionStart, "width: " + sizeSet + "px;");
+	$('.' + classListener).data(scrollPositionEnd, "width: " + doubleSizeSet + "px;");
+}
+
+$('.scr-ani-dot').data('1', 'hello');
+
+
+console.log($('.scr-ani-dot').data('1'));
+
