870d7c6d18d90f36301efc7eab1ee207b633c18b
Aditya Viswanathan <aditya@adityaviswanathan.com>
2013-08-05 18:40:59 -0700

    right, left, down motions achieved using two add classes

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-scroll-animation.js
-===================
+REPO README
 
-Animation library based on scroll.
+TO START:
+
+1. linGrowth function
+
+	a) Initialize with div:
+
+		ex. <div class="test"></div>
+
+	b) Assign animation parameters to div (in JS):
+
+		ex. <script>
+
+			//linGrowthRight(class to assign, class to add, size of unit, speed of animation, distance for animation to travel, color of unit)
+
+			linGrowthRight('test', 'scr-ani-dot', 5, 0.25, 50, '#ddd');
+
+			</script>
+
+	c) Using 'scr-ani-dot-right' will align animation to the right:
+	
+		ex. <script>
+
+			//linGrowthRight(class to assign, class to add, size of unit, speed of animation, distance for animation to travel, color of unit)
+
+			linGrowthRight('test', 'scr-ani-dot-right', 5, 0.25, 50, '#ddd');
+
+			</script>		
+
+
+
--- scripts/scroll-animation/scroll-animation.js
+++ scripts/scroll-animation/scroll-animation.js
@@ -1,9 +1,19 @@
 
 var globalPosition = 0;
 
-function linGrowthRight(classListener, classAdd, size, speed, distance, color) { //class to apply settings to, class to create (custom), size of unit, low = fast, length of extension, color of unit
+function linGrowthHorizontal(classListener, classAdd, size, speed, distance, color) { //class to apply settings to, class to create (custom), size of unit, low = fast, length of extension, color of unit
 	
 	$('.' + classListener).addClass(classAdd);
+
+	var sizeSet = distance * 10;
+
+	var doubleSizeSet = 2 * sizeSet;
+
+	var unitSize = size * 10;	
+
+	$('.' + classListener).css('height', unitSize+'px');
+	$('.' + classListener).css('width', unitSize+'px');
+	$('.' + classListener).css('background-color', color);
 
 	globalPosition = $(window).scrollTop();
 
@@ -13,23 +23,47 @@ function linGrowthRight(classListener, classAdd, size, speed, distance, color) {
 
 	var sizeInit = $("." + classListener).width();
 
+	$('.' + classListener).attr('data-' + scrollPositionStart, "width:" + unitSize + "px;");
+	$('.' + classListener).attr('data-' + scrollPositionEnd, "width:" + doubleSizeSet + "px;");
+	console.log(unitSize);
+
+}
+
+function linGrowthVertical(classListener, classAdd, size, speed, distance, color) { //class to apply settings to, class to create (custom), size of unit, low = fast, length of extension, color of unit
+	
+	$('.' + classListener).addClass(classAdd);
+
 	var sizeSet = distance * 10;
 
 	var doubleSizeSet = 2 * sizeSet;
 
-	var unitSize = size * 10;
+	var unitSize = size * 10;	
 
 	$('.' + classListener).css('height', unitSize+'px');
 	$('.' + classListener).css('width', unitSize+'px');
+	$('.' + classListener).css('background-color', color);
 
-	$( "." + classAdd).attr('data-' + scrollPositionStart, "width:" + unitSize + "px;");
-	$( "." + classAdd ).attr('data-' + scrollPositionEnd, "width:" + doubleSizeSet + "px;");
+	globalPosition = $(window).scrollTop();
+
+	var scrollPositionStart = globalPosition;
+
+	var scrollPositionEnd = scrollPositionStart + (speed * 100);
+
+	var sizeInit = $("." + classListener).width();
+
+	$('.' + classListener).attr('data-' + scrollPositionStart, "height:" + unitSize + "px;");
+	$('.' + classListener).attr('data-' + scrollPositionEnd, "height:" + doubleSizeSet + "px;");
+	console.log(unitSize);
 
 }
 
-linGrowthRight('scr-ani-dot-1', 'scr-ani-dot-new-1', 5, 0.25, 50, '#333333');
+linGrowthHorizontal('scr-ani-dot-1', 'scr-ani-dot', 3, 0.25, 50, '#ddd');
 
-linGrowthRight('scr-ani-dot-2', 'scr-ani-dot-new-2',  1, 0.25, 1, '#333333');
+linGrowthHorizontal('scr-ani-dot-2', 'scr-ani-dot-right',  0.5, 1, 100, '#333333');
+
+linGrowthVertical('scr-ani-dot-3', 'scr-ani-dot-right',  1.5, 1, 30, '#009974');
+
+linGrowthVertical('scr-ani-dot-4', 'scr-ani-dot',  1.5, 1, 30, '#bcd4d4');
 
 
 skrollr.init({ smoothScrolling: true });
