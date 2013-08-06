90c56f7cab43007f77afdd166fb9abbdbc548f18
Aditya Viswanathan <aditya@adityaviswanathan.com>
2013-08-05 20:01:01 -0700

    added code snippet section with highlighting hover functionality

--- scripts/scroll-animation/scroll-animation.js
+++ scripts/scroll-animation/scroll-animation.js
@@ -25,7 +25,6 @@ function linGrowthHorizontal(classListener, classAdd, size, speed, distance, col
 
 	$('.' + classListener).attr('data-' + scrollPositionStart, "width:" + unitSize + "px;");
 	$('.' + classListener).attr('data-' + scrollPositionEnd, "width:" + doubleSizeSet + "px;");
-	console.log(unitSize);
 
 }
 
@@ -53,17 +52,16 @@ function linGrowthVertical(classListener, classAdd, size, speed, distance, color
 
 	$('.' + classListener).attr('data-' + scrollPositionStart, "height:" + unitSize + "px;");
 	$('.' + classListener).attr('data-' + scrollPositionEnd, "height:" + doubleSizeSet + "px;");
-	console.log(unitSize);
 
 }
 
-linGrowthHorizontal('scr-ani-dot-1', 'scr-ani-dot', 3, 0.25, 50, '#ddd');
+linGrowthHorizontal('scr-ani-dot-1', 'scr-ani-circle', 3, 0.25, 20, '#ddd');
 
-linGrowthHorizontal('scr-ani-dot-2', 'scr-ani-dot-right',  0.5, 1, 100, '#333333');
+linGrowthHorizontal('scr-ani-dot-2', 'scr-ani-dot-right',  0.5, 1, 30, '#333333');
 
-linGrowthVertical('scr-ani-dot-3', 'scr-ani-dot-right',  1.5, 1, 30, '#009974');
+linGrowthVertical('scr-ani-dot-3', 'scr-ani-circle-right',  1.5, 1, 10, '#009974');
 
-linGrowthVertical('scr-ani-dot-4', 'scr-ani-dot',  1.5, 1, 30, '#bcd4d4');
+linGrowthVertical('scr-ani-dot-4', 'scr-ani-dot',  1.5, 1, 8, '#bcd4d4');
 
 
 skrollr.init({ smoothScrolling: true });
---
+++ scripts/template.js
@@ -0,0 +1,44 @@
+var counter = 0;
+var highlightArray
+
+// $('.lin-growth col-lg-7').children().each(function() {
+//   highlightArray = $(this)[counter];
+//   counter = counter + 1;
+// });
+
+var colorHold;
+
+$('.effect-listen').hover(function() {
+	console.log('hi');
+	if($(this).hasClass('effect-1')) {
+		colorHold = $('.scr-ani-dot-1').css('background-color');
+		$(this).css('background-color', '#f2f0c9');
+		$('.scr-ani-dot-1').css('background-color', '#f2f0c9');
+	} else if($(this).hasClass('effect-2')){
+		colorHold = $('.scr-ani-dot-2').css('background-color');
+		$(this).css('background-color', '#f2f0c9');
+		$('.scr-ani-dot-2').css('background-color', '#f2f0c9');
+	} else if($(this).hasClass('effect-3')) {
+		colorHold = $('.scr-ani-dot-3').css('background-color');
+		$(this).css('background-color', '#f2f0c9');
+		$('.scr-ani-dot-3').css('background-color', '#f2f0c9');
+	} else if($(this).hasClass('effect-4')) {
+		colorHold = $('.scr-ani-dot-4').css('background-color');
+		$(this).css('background-color', '#f2f0c9');
+		$('.scr-ani-dot-4').css('background-color', '#f2f0c9');
+	}
+}, function() {
+	if($(this).hasClass('effect-1')) { 
+		$(this).css('background-color', '#fff');
+		$('.scr-ani-dot-1').css('background-color', colorHold);
+	} else if ($(this).hasClass('effect-2')) { 
+		$(this).css('background-color', '#fff');
+		$('.scr-ani-dot-2').css('background-color', colorHold);
+	} else if ($(this).hasClass('effect-3')) { 
+		$(this).css('background-color', '#fff');
+		$('.scr-ani-dot-3').css('background-color', colorHold);
+	} else if ($(this).hasClass('effect-4')) { 
+		$(this).css('background-color', '#fff');
+		$('.scr-ani-dot-4').css('background-color', colorHold);
+	}
+});
